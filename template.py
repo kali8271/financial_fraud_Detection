395c41c72a99db8810ae58851ede1ae26626804e
KALIMULLAH ANSARI <akalimullah900@gmail.com>
2025-06-13 23:01:35 +0530

    created

--- template.py
+++ template.py
@@ -13,3 +13,13 @@ structure = {
     ],
     "": ["main.py", "requirements.txt", "README.md"]
 }
+
+for folder, files in structure.items():
+    if folder and not os.path.exists(folder):
+        os.makedirs(folder)
+    for file in files:
+        filepath = os.path.join(folder, file) if folder else file
+        with open(filepath, "w") as f:
+            f.write(f"# {file}\n\n")
+
+print("✅ Folders and template files created.")
