6b519d8bf09df81ffa425e821d02c66c983f53ba
KALIMULLAH ANSARI <akalimullah900@gmail.com>
2025-06-13 22:58:17 +0530

    created

---
+++ template.py
@@ -0,0 +1,15 @@
+import os
+
+structure = {
+    "data": [],
+    "notebooks": [],
+    "src": [
+        "data_loader.py",
+        "preprocessing.py",
+        "model_training.py",
+        "evaluation.py",
+        "utils.py",
+        "__init__.py"
+    ],
+    "": ["main.py", "requirements.txt", "README.md"]
+}
